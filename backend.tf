b6e6a1bfdaa2c6120ee9c5f643de92935ba6984a
Miko Meller <miko.meller@cloudreach.com>
2022-10-11 10:47:11 +0200

    populated github actions with credentials set on aws iam, filled in .github/workflows/deploy-infra.yml, tf-plan

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
     bucket = "talent-academy-terraform-tfstates-787786425565"
-    key    = "sprint1/week2/tf-vpc-lab/terraform.tfstates"
+    key    = "sprint1/week5/tf-vpc-ci-cd/terraform.tfstates"
   }
 }
